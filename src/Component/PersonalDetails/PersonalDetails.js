57ffbb30861909bd1a88cd90f127d7541ece971b
mahmuda <mahmudazaman0509@gmail.com>
2018-12-24 15:21:02 +0600

    working

---
+++ src/Component/PersonalDetails/PersonalDetails.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PersonalDetails extends Component {
+  render() {
+    return (
+      <div>
+        <p>Do you want to share your personal details?</p>
+      </div>
+    );
+  }
+}
+
+export default PersonalDetails;
---
+++ src/Component/SelectLocation/SelectLocation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SelectLocation extends Component {
+  render() {
+    return (
+        <div>
+            <p>Please select a location</p>
+        </div>
+    );
+  }
+}
+
+export default SelectLocation;
